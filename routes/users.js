f1cc1d215a52ad6227513e41015ea0373720318d
Hamed <sghavamnia@cs.stonybrook.edu>
2020-10-17 15:06:11 -0400

    added db connection

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var pg = require('pg');
+
+var connectionString = "host=cus-pgsql-prd.postgres.database.azure.com port=5432 dbname=demo user=kmicro@cus-pgsql-prd password=mdef@tpY1824;";
+
+var pgClient = new pg.Client(connectionString);
+
+pgClient.connect();
+
+var query = pgClient.query("SELECT id from customers where name = 'hamed'");
+
+query.on("row", function(row,result){
+
+result.addRow(row);
+
+});
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
