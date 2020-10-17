5866a7498501acc1d871d446c4d0a5b38a3608f2
Hamed <sghavamnia@cs.stonybrook.edu>
2020-10-17 15:11:02 -0400

    removed db connection

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var pg = require('pg');
-
-var connectionString = "host=cus-pgsql-prd.postgres.database.azure.com port=5432 dbname=demo user=kmicro@cus-pgsql-prd password=mdef@tpY1824;";
-
-var pgClient = new pg.Client(connectionString);
-
-pgClient.connect();
-
-var query = pgClient.query("SELECT id from customers where name = 'hamed'");
-
-query.on("row", function(row,result){
-
-result.addRow(row);
-
-});
+//var pg = require('pg');
+//
+//var connectionString = "host=cus-pgsql-prd.postgres.database.azure.com port=5432 dbname=demo user=kmicro@cus-pgsql-prd password=mdef@tpY1824;";
+//
+//var pgClient = new pg.Client(connectionString);
+//
+//pgClient.connect();
+//
+//var query = pgClient.query("SELECT id from customers where name = 'hamed'");
+//
+//query.on("row", function(row,result){
+//
+//result.addRow(row);
+//
+//});
 
 
 /* GET users listing. */
